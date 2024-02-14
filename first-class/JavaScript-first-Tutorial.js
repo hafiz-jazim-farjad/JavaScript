87e387a014814bffef9987b9cbd03abc0bda511d
Jazim Farjad <jazim52946@gmail.com>
2024-02-14 14:29:49 +0500

    Created

---
+++ first-class/JavaScript-first-Tutorial.js
@@ -0,0 +1,19 @@
+// alert("Hello");
+
+// var number1 = 4;
+// var number2 = 6;
+
+// alert(number1 + number2 * number2 / number1);
+// BODMAS
+
+// Bracket Open , Multification , Addition , Subtraction
+
+
+var UserName = prompt("Enter Your Name");
+var UserAge = prompt("Enter Your Current Age");
+var UserDate = prompt("Enter Your BirthDate")
+var UserMonth = prompt("Enter Your Birth Month");
+
+var age_result = 2024 - UserAge ; 
+
+document.write("Your Name is " + UserName + " Your Birth Year is " + UserDate + "-" + UserMonth + "-" , age_result);
---
+++ third-class/index.js
@@ -0,0 +1,41 @@
+// method one 
+// let a = prompt("Enter the value for 'a' :");
+// let b = prompt("Enter the value for 'b':");
+// let num1 = Number(a)
+// let num2 = Number(b)
+// let result = num1 + num2
+// console.log(result);
+
+
+// method two 
+// let a = Number(prompt("Enter the value for 'a':"));
+// let b = Number(prompt("Enter the value for 'b':"));
+// let result = a + b;
+// console.log(result);
+
+
+// third method 
+// let a = parseInt(prompt("Enter the value for 'a':"));
+// let b = parseInt(prompt("Enter the value for 'b':"));
+// let c = parseInt(prompt("Enter the value for 'c':"));
+// let result = a + b + c;
+// console.log(result);
+
+// fourth method 
+// let a = +prompt("Enter the value for 'a':");
+// let b = +prompt("Enter the value for 'b':");
+// let c = +prompt("Enter the value for 'c':");
+// let result = a + b + c;
+// console.log(result);
+
+
+// if else condition 
+
+let ForRollNumber = Number(prompt("Please enter your Roll number: "));
+if (ForRollNumber == 55) {
+    // document.getElementById('demo').innerHTML =  '<h3>Hello Mr. Abdelrhman Ahmed</h3>';
+    console.log(ForRollNumber + " is in Computer Science Department.");
+}
+else{
+    console.log(ForRollNumber + " is in Biology Department.");
+}
